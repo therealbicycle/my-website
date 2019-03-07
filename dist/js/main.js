4cd6180af24f3a9583a14ff6597bec34c5531e2e
Wilfred Magnussen <wilfrodger@hotmail.com>
2019-03-07 11:12:03 +0000

    final draft, added university briefs section and added all videos and descriptions

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,17 +31,16 @@ $('#view-projects').on('click', function () {
     );
 });
 
-// Smooth scroll to contact info
-//$('#contact-me').on('click', function() {
-//    const footer = $('#footer').position().top;
-//
-//    $('html, body').animate( {
-//            scrollTop: footer
-//            scrollTop: $(document).height()
-//        },
-//        'slow'
-//   );
-//});
+// Smooth scroll to projects section
+$('#view-projects').on('click', function () {
+    const projects = $('#projects-section').position().top;
+
+    $('html, body').animate({
+        scrollTop: projects
+    },
+        'slow'
+    );
+});
 
 $('#contact').click(function () {
     $('html, body').animate({
